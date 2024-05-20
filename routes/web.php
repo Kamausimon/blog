9570b6ee0770dcc41af317656a072f97fdf26f61
kamau simon <kamausimon217@gmail.com>
2024-05-20 12:44:48 +0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/register', function () {
+    return view('register');
+});
+Route::get('/login', function () {
+    return view('login');
+});
